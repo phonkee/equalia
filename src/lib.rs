9deeebed1018fb471e9a305782b45fc9ebd59633
phonkee <phonkee@phonkee.eu>
2021-03-18 20:50:42 +0100

    blank impl done

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 use darling::*;
 use proc_macro2::{Ident, TokenStream as SynTokenStream};
 use proc_macro::{TokenStream, Span};
-use syn::{DeriveInput, parse_macro_input, Data, DataStruct};
+use syn::{DeriveInput, parse_macro_input, Data, DataStruct, Type};
 use quote::*;
 
 #[cfg(feature = "nightly")]
@@ -17,25 +17,53 @@ fn error(_: Span, data: &str) -> SynTokenStream {
     quote! { compile_error!(#data); }
 }
 
-#[derive(Default, FromMeta)]
-#[darling(default)]
+#[derive(Debug, FromDeriveInput)]
+#[darling(attributes(equalia), supports(struct_named))]
 struct Equalia {
+    ident: Ident,
+    data: ast::Data<util::Ignored, EqualiaField>,
+    #[darling(default)]
     hash: bool,
 }
 
-#[derive(FromField)]
+#[derive(Debug, FromField)]
 #[darling(attributes(equalia), forward_attrs(doc, allow, warn))]
-struct Attribute {
+struct EqualiaField {
+    ident: Option<Ident>,
+    ty: Type,
+
+    #[darling(default)]
     skip: bool,
+
+    #[darling(default)]
     only: bool,
+
+    #[darling(default)]
     map: Option<Ident>,
 }
 
 /// generate actual equalia implementations
 fn generate_equalia(_input: &DeriveInput, _data: &DataStruct) -> std::result::Result<SynTokenStream, SynTokenStream> {
     println!("fields: {:?}", _data.fields);
-    Ok(SynTokenStream::new())
+    let eq_attrs: Equalia = match FromDeriveInput::from_derive_input(_input) {
+        Ok(v) => v,
+        Err(e) => return Err(e.write_errors()),
+    };
+
+
+    println!("equalia: {:?}", eq_attrs.ident);
+
+    let i = eq_attrs.ident;
+
+    Ok(quote! {
+        impl PartialEq for #i {
+            fn eq(&self, other: &Self) -> bool {
+                todo!()
+            }
+        }
+    })
 }
+
 
 #[proc_macro_derive(Equalia, attributes(equalia))]
 pub fn derive_equalia(input: TokenStream) -> TokenStream {
